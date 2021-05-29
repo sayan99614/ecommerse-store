1fef543bfeb0ec671f4fe421808ad9218f6919c0
dheeman pati <dheemanpati53@gmail.com>
2021-05-29 15:47:20 +0530

    added jwt and validation

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,9 +1,21 @@
 const User = require("../models/User");
+const Jwt = require("jsonwebtoken");
+const Expressjwt = require("express-jwt");
+const { validationResult } = require("express-validator");
 exports.signOut = (req, res) => {
   res.json({ message: "signout successfully 👍" });
 };
 exports.signUp = async (req, res) => {
   try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const err = {
+        errmsg: errors.array(),
+      };
+      return res.status(422).json(err);
+    }
+
     const user = new User(req.body);
     await user.save((error, user) => {
       if (error) {
@@ -18,4 +30,34 @@ exports.signUp = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+exports.signIn = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ error: errors.array() });
+    }
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(400).json({ error: "user email doesnot exist" });
+    }
+
+    if (!user.authenticate(req.body.password)) {
+      return res.status(401).json({ error: "email and password mismatched" });
+    }
+    const token = Jwt.sign({ _id: user._id }, process.env.SECRET);
+    res.cookie("token", token, { expire: new Date() + 9999 });
+    const { _id, name, email, role } = user;
+    return res.json({
+      token,
+      user: {
+        _id,
+        name,
+        email,
+        role,
+      },
+    });
+  } catch (error) {}
 };
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,8 +1,44 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
-const { signOut, signUp } = require("../controllers/Auth");
+const { signOut, signUp, signIn } = require("../controllers/Auth");
 
 router.get("/", signOut);
-router.post("/signup", signUp);
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("name must be at least 3 characters"),
+    check("lastname")
+      .isLength({ min: 3 })
+      .withMessage("you must provide a lastname"),
+    check("email")
+      .isEmail()
+      .withMessage("email should be in proper format eg: example@gmail.com"),
+    check("password")
+      .isLength({ min: 8, max: 15 })
+      .isStrongPassword()
+      .withMessage(
+        "Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character"
+      ),
+  ],
+  signUp
+);
+
+router.post(
+  "/signin",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("email should be in proper format eg: example@gmail.com"),
+    check("password")
+      .isLength({ min: 8 })
+      .withMessage("password should not be empty")
+      .isStrongPassword()
+      .withMessage("please change your password as soon as possible"),
+  ],
+  signIn
+);
 
 module.exports = router;
